44f3e03a428930abdd89f326bc2bba943e321bbb
throwbackjams <yli17@alumni.nd.edu>
2022-04-23 11:38:48 -0500

    complete onchain program - contract section & deploy to testnet

--- keys.terrain.js
+++ keys.terrain.js
@@ -1,5 +1,4 @@
-// can use `process.env.SECRET_MNEMONIC` or `process.env.SECRET_PRIV_KEY`
-// to populate secret in CI environment instead of hardcoding
+require('dotenv').config();
 
 module.exports = {
   custom_tester_1: {
@@ -8,5 +7,8 @@ module.exports = {
   },
   custom_tester_2: {
     privateKey: "fGl1yNoUnnNUqTUXXhxH9vJU0htlz9lWwBt3fQw+ixw=",
+  },
+  bombay: {
+    mnemonic: process.env.SEED_PHRASE,
   },
 };
